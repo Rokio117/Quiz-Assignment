df258799a1e87a405b156fcc5b39357cdc5f7c25
Nick Hillary <hillan02@gmail.com>
2019-06-03 20:39:37 -0500

    Updated Questions and style sheets

---
+++ index.js
@@ -0,0 +1,54 @@
+function resetButton() {
+  console.log('resetButton ran');
+  //Resets everything(all answers, score,question#), sends user back to intro page
+}
+
+function insertQuestionText() {
+  console.log('insertQuestionText ran');
+  //inputs the current question into the 'question _ of 10 span
+}
+
+function updateScore() {
+  console.log('updateScore ran');
+  //updates the user's current score in the Score: _/10 span
+}
+
+function fillQuestion() {
+  console.log('fillQuestion ran');
+  //updates the text of question to the one the user should be on
+}
+
+function fillAnswers() {
+  console.log('fillAnswers ran');
+  //updates the text of each answer to fit the question the user is currently on
+}
+
+function submitAnswer() {
+  console.log('submitAnswer ran');
+  //updates user score on page and in object array
+  //generates appropriate text for next page depending on user answer
+  //sends user to result page
+}
+
+function continueButton() {
+  console.log('continueButton ran');
+  //Sets question and answer text for the next page? (or do we want submitAnswer to do this?
+  //sends user to next question page
+  //if question 10, sends user to final score page
+}
+
+function callAllFunctions() {
+  resetButton();
+  insertQuestionText();
+  updateScore();
+  fillQuestion();
+  fillAnswers();
+  submitAnswer();
+  continueButton();
+  //calls all functions
+}
+
+$(callAllFunctions);
+
+
+
---
+++ questions.js
@@ -0,0 +1,116 @@
+const STORE = [
+  {
+    number: 1,
+    question: "Name the four basic instrument families",
+    answers: {
+      A: "Woodwinds, Strings, Brass, Percussion",
+      B: "Winds, Metal, Shaker, Slappy",
+      C: "Keyboard, Valve, Key, Contact",
+      D: "Big, Small, Medium, over-easy"
+    },
+    correct: "A"
+  },
+  {
+    number: 2,
+    question:
+      "Which one of these instruments is a member of the Woodwind family?",
+    answers: {
+      A: "Trombone",
+      B: "Harpsichord",
+      C: "Viola",
+      D: "Bassoon"
+    },
+    correct: "D"
+  },
+  {
+    number: 3,
+    question: "Select the four main voice types",
+    answers: {
+      A: "Woman, Man, Boy, Girl",
+      B: "Gravely, Smooth, piercing, flat",
+      C: "Soprano, Alto, Tenor, Bass",
+      D: "High, Low, Medium-High, Medium-Low"
+    },
+    correct: "C"
+  },
+  {
+    number: 4,
+    question: "Which one of these voice types is the highest?",
+    answers: {
+      A: "Alto",
+      B: "Contralto",
+      C: "Mezzo-soprano",
+      D: "Soprano"
+    },
+    correct: "D"
+  },
+  {
+    number: 3,
+    question: "Which one of the following is an element of music?",
+    answers: {
+      A: "Instruments",
+      B: "Singing",
+      C: "Dynamics",
+      D: "Transposing"
+    },
+    correct: "C"
+  },
+  {
+    number: 6,
+    question: "Which one of these terms is not typically found in music?",
+    answers: {
+      A: "Forte",
+      B: "Largo",
+      C: "Follemente",
+      D: "Ritardando"
+    },
+    correct: "C"
+  },
+  {
+    number: 7,
+    question: "Which one of these musical periods is the earliest?",
+    answers: {
+      A: "Classical",
+      B: "Baroque",
+      C: "Modern",
+      D: "Romantic"
+    },
+    correct: "B"
+  },
+  {
+    number: 8,
+    question: "Which one of these composers was born first?",
+    answers: {
+      A: "Johann Sebastian Bach",
+      B: "Ludwig van Beethoven",
+      C: "Igor Stravinsky",
+      D: "John Philip Sousa"
+    },
+    correct: "A"
+  },
+  {
+    number: 9,
+    question: "How many symphonies did Beethoven write?",
+    answers: {
+      A: "3",
+      B: "9",
+      C: "48",
+      D: "OVER 9000!!!!!!!"
+    },
+    correct: "B"
+  },
+  {
+    number: 10,
+    question:
+      "Which one of these instruments was used primarily in the band “Jethro Tull”?",
+    answers: {
+      A: "Clarinet",
+      B: "Chainsaw",
+      C: "Flute",
+      D: "Flamethrower"
+    },
+    correct: "C"
+  }
+];
+
+console.log(STORE);
